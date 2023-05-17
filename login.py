192b03fb8126f3e8a2049ca71dfef65491c83589
Deivanai S <86340024+Deivanai-Subramanian@users.noreply.github.com>
2023-05-17 10:55:19 +0530

    Update login.py

--- login.py
+++ login.py
@@ -17,3 +17,29 @@ if LOGGED_IN == True:
 
 if st.session_state['LOGGED_IN'] == True:
 	st.write("WELCOME!")
+        selected = option_menu(
+            menu_title=None,  # required
+            options=["Home", "Projects", "Contact"],  # required
+            icons=["house", "book", "envelope"],  # optional
+            menu_icon="cast",  # optional
+            default_index=0,  # optional
+            orientation="horizontal",
+            styles={
+                "container": {"padding": "0!important", "background-color": "#fafafa"},
+                "icon": {"color": "orange", "font-size": "25px"},
+                "nav-link": {
+                    "font-size": "25px",
+                    "text-align": "left",
+                    "margin": "0px",
+                    "--hover-color": "#eee",
+                },
+                "nav-link-selected": {"background-color": "green"},
+            },
+        )
+        if selected == "Home":
+	        st.write("WELCOME TO HOME")
+        elif selected == "Projects":
+	        st.write("WELCOME TO PROJECTS")
+        elif selected == "Contact":
+	        st.write("WELCOME TO CONTACT")
+

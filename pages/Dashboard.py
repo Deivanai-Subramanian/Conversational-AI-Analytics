3d915112527b39758c5b855798684cb70365aa3a
Deivanai S <86340024+Deivanai-Subramanian@users.noreply.github.com>
2023-05-21 13:10:31 +0530

    Update Dashboard.py

--- pages/Dashboard.py
+++ pages/Dashboard.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout
 
 
 # ---- SIDEBAR ----
-st.sidebar.header("Filter Here:")
+st.subheader("Filter Here:")
 Main_category = st.multiselect(
     "Select the Main Category:",
     options=df["main_category"].unique(),

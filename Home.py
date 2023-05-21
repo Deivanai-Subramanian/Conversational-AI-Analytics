40ce0de99713217ff8888f85794bda8228ed53d8
Deivanai S <86340024+Deivanai-Subramanian@users.noreply.github.com>
2023-05-22 00:11:59 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,8 +11,14 @@ __login__obj = __login__(auth_token = "pk_prod_27GNYP246PME2HPJZEV7FX35XRX5",
 LOGGED_IN = __login__obj.build_login_ui()
 
 if LOGGED_IN == True:
-	st.markdown("Your Streamlit Application Begins here!")
+	st.markdown("Streamlit Application Begins here!")
 
 if st.session_state['LOGGED_IN'] == True:
 	st.title("CONVERSATIONAL AI ANALYTICS")
 	st.image('https://www.cxtoday.com/wp-content/uploads/2022/04/conversational-analytics.jpg')
+	st.write("created by TEAM TECH ASPIRANTS")
+	st.write("This Web App contains four modules:")
+	st.write("1. Home
+		 2. Chatbot
+		 3. Dashboard
+		 4. Product Details")
